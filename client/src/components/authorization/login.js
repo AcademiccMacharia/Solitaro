a499ee3576e6fdf7b3771e4b9e3b0eb65ed480cc
AcademiccMacharia <machariab19@gmail.com>
2023-07-09 14:14:38 +0300

    implementing login form

--- client/src/components/authorization/login.js
+++ client/src/components/authorization/login.js
@@ -3,78 +3,134 @@ import './auth.css';
 import landingLogo from '../../assets/login-logo.png';
 import connect from '../../assets/connecting.svg';
 import interact from '../../assets/conversations.svg';
-import loving from '../../assets/loving.svg';
+import loving from '../../assets/selfie.png';
+import { FaArrowRight, FaFacebook, FaGoogle, FaTwitter } from 'react-icons/fa';
+import { RiEyeFill, RiEyeOffFill } from 'react-icons/ri';
 
 const Login = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [autoplayActive, setAutoplayActive] = useState(true);
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [autoplayActive, setAutoplayActive] = useState(true);
+    const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const slideData = [
-    {
-      image: connect,
-      caption: "Connect with like-minded individuals, fostering meaningful relationships in a thriving social community",
-    },
-    {
-      image: interact,
-      caption: "Interact, engage, and spark conversations with diverse voices",
-    },
-    {
-      image: loving,
-      caption: "Embark on an exciting journey where laughter and enjoyment are the heartbeat of your social media experience",
-    },
-  ];
-
-  useEffect(() => {
-    let interval;
-
-    if (autoplayActive) {
-      interval = setInterval(() => {
-        setCurrentSlide((prevSlide) => (prevSlide + 1) % slideData.length);
-      }, 3000);
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
     }
 
-    return () => clearInterval(interval);
-  }, [autoplayActive, slideData.length]);
 
-  const handleDotClick = (index) => {
-    setCurrentSlide(index);
-    setAutoplayActive(false);
-  };
+    const slideData = [
+        {
+            image: connect,
+            caption: "Connect with like-minded individuals, fostering meaningful relationships in a thriving social community",
+        },
+        {
+            image: interact,
+            caption: "Interact, engage, and spark conversations with diverse voices",
+        },
+        {
+            image: loving,
+            caption: "Embark on an exciting journey where laughter and enjoyment are the heartbeat of your social media experience",
+        },
+    ];
 
-  return (
-    <div className='login-page'>
-      <div className='login-left'>
-        <img src={landingLogo} className='my-logo' alt='mylogo' />
-        <div className='image-slider'>
-          {slideData.map((slide, index) => (
-            <div className={`slide ${index === currentSlide ? 'active' : ''}`} key={index}>
-              <img src={slide.image} alt='slide' />
-              <div className='slide-info'>
-                <p>{slide.caption}</p>
-              </div>
+    useEffect(() => {
+        let interval;
+
+        if (autoplayActive) {
+            interval = setInterval(() => {
+                setCurrentSlide((prevSlide) => (prevSlide + 1) % slideData.length);
+            }, 3000);
+        }
+
+        return () => clearInterval(interval);
+    }, [autoplayActive, slideData.length]);
+
+    const handleDotClick = (index) => {
+        setCurrentSlide(index);
+        setAutoplayActive(true)
+    };
+
+    return (
+        <div className='login-page'>
+            <div className='login-left'>
+                <img src={landingLogo} className='my-logo' alt='mylogo' />
+                <div className='image-slider'>
+                    {slideData.map((slide, index) => (
+                        <div className={`slide ${index === currentSlide ? 'active' : ''}`} key={index}>
+                            <img src={slide.image} alt='slide' />
+                            <div className='slide-info'>
+                                <p>{slide.caption}</p>
+                            </div>
+                        </div>
+                    ))}
+                    <div className='navigation'>
+                        {slideData.map((_, index) => (
+                            <div
+                                className={`dot ${index === currentSlide ? 'active' : ''}`}
+                                key={index}
+                                onClick={() => handleDotClick(index)}
+                            ></div>
+                        ))}
+                    </div>
+                </div>
+                <div className='left-bottom'>
+                    <p>&copy; 2023 Solitaro. All rights reserved.</p>
+                </div>
             </div>
-          ))}
-          <div className='navigation'>
-            {slideData.map((_, index) => (
-              <div
-                className={`dot ${index === currentSlide ? 'active' : ''}`}
-                key={index}
-                onClick={() => handleDotClick(index)}
-              ></div>
-            ))}
-          </div>
+            <div className='login-right'>
+                <div className='login-header'>
+                    <h1>Hello Again!</h1>
+                    <p>Login and continue your journey with us</p>
+                </div>
+                <div className='login-form'>
+                    <form>
+                        <div className='form-group'>
+                            <label htmlFor='email'>Email:</label>
+                            <br></br>
+                            <input type='email' name='email' placeholder='Enter your email' />
+                            <span></span>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='password'>Password:</label>
+                            <br></br>
+                            <div className='password-input-wrapper'>
+                                <input type={passwordVisible ? 'text' : 'password'} name='password' placeholder='Enter your password' />
+                                <i className='eye-icon' onClick={togglePasswordVisibility}>
+                                    {passwordVisible ? <RiEyeFill size={24}/> : <RiEyeOffFill size={20}/>}
+                                </i>
+                            </div>
+                        </div>
+                        <div className='form-group'>
+                            <button type='submit'><FaArrowRight /></button>
+                        </div>
+                        <div className='form-group'>
+                            <p>Don't have an account? <a href='/signup'>Sign up</a></p>
+                        </div>
+                    </form>
+                </div>
+                <div className='right-bottom'>
+                    <div className="line-container">
+                        <hr className="line"></hr>
+                        <span className="or">or</span>
+                        <hr className="line"></hr>
+                    </div>
+                    <div className='social-login'>
+                        <button className='social-btn'>
+                            <i className='fab fa-facebook-f'> <FaFacebook /> </i>
+                            <span>Continue with Facebook</span>
+                        </button>
+                        <button className='social-btn'>
+                            <i className='fab fa-google'> <FaGoogle /></i>
+                            <span>Continue with Google</span>
+                        </button>
+                        <button className='social-btn'>
+                            <i className='fab fa-twitter'> <FaTwitter /> </i>
+                            <span>Continue with Twitter</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className='left-bottom'>
-        <p>&copy; 2023 Solitaro. All rights reserved.</p>
-        </div>
-      </div>
-      <div className='login-right'>
-        <div>
-          <button>Sign In</button> <button>Sign Up</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
