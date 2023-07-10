a566a07cfa7fd6a45bd046d70e2b2ca785becd0a
AcademiccMacharia <machariab19@gmail.com>
2023-07-10 06:34:46 +0300

    handling middleware

--- server/auth/src/middlewares/sessionAuthorization.js
+++ server/auth/src/middlewares/sessionAuthorization.js
@@ -3,11 +3,8 @@ function sessionAuthorization(req, res, next) {
   
     if (req.session && authorized) {
       console.log(req.session)
-      // Session is valid and authorized
-      
-      next(); // Proceed to the next middleware or route handler
+      next();
     } else {
-      // Session is invalid or unauthorized
       res.status(401).send("You are not logged in");
     }
   }
